10a1782850a00b08cf0ff25ee6fce4742e120c54
Walid <walid.djebara3@gmail.com>
2023-10-24 14:24:34 +0200

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Counter from "./pages/Counter/counter"
 import Fruity from "./pages/Fruity/Fruity"
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-700 px-4">
       <div className="mx-auto max-w-3xl pt-10 pb-20">
-
-        {/* 1.Counter app */}
-        {/* <Counter /> */}
 
         {/* 2.Fruity app */}
         <Fruity />
--- src/store.js
+++ src/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counter from "./features/counter";
 import fruits from "./features/fruity";
 import fruitsCart from "./features/fruitsCart";
 
 export const store = configureStore({
     reducer: {
-        counter,
         fruits,
         fruitsCart
     }
